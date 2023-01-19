3876a53fa41ce12b16f342067e8ff3d4c223ea5a
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-01-19 20:50:55 +0100

    Update DSM-51-CLOCK-IN-ASM.asm

--- DSM-51-CLOCK-IN-ASM/DSM-51-CLOCK-IN-ASM.asm
+++ DSM-51-CLOCK-IN-ASM/DSM-51-CLOCK-IN-ASM.asm
@@ -9,8 +9,8 @@ SEG7 bit P1.6
 SKEY bit P3.5
 
 ; DISPLAY
-ACTIVE_DISPLAY_BUFFER equ 30h 				; ADRES REJESTRU AKTYWNYCH WYåWIETLACZY
-ACTIVE_DISPLAY_SEGMENTS_BUFFER equ 38h      ; ADRES REJESTRU AKTYWNYCH SEGMENT”W
+ACTIVE_DISPLAY_BUFFER equ 30h 				; ADRES REJESTRU AKTYWNYCH WY≈öWIETLACZY
+ACTIVE_DISPLAY_SEGMENTS_BUFFER equ 38h      ; ADRES REJESTRU AKTYWNYCH SEGMENT√ìW
 
 ; KEYBOARD
 KEYBOARD  equ 7Ch                           ; 4 OSTATNIE STANY KLAWIATURY
@@ -25,10 +25,10 @@ HH    equ 73h				                ; LICZNIK GODZIN
 org 0
     ljmp start
 
-org 0Bh				                        ; PROCEDURA OBS£UGI PRZERWANIA OD TIMER0
-    mov TH0, #224           	            ; DLA TRYBU 0 - 900 PRZERWA— NA SEKUND  CO 1024 CYKLE MASZYNOWE
+org 0Bh				                        ; PROCEDURA OBS≈ÅUGI PRZERWANIA OD TIMER0
+    mov TH0, #224           	            ; DLA TRYBU 0 - 900 PRZERWA≈É NA SEKUNDƒò CO 1024 CYKLE MASZYNOWE
     setb F0
-    reti            		                ; POWR”T Z PRZERWANIA
+    reti            		                ; POWR√ìT Z PRZERWANIA
 
 org 80h
 
@@ -40,53 +40,53 @@ start:
 
     lcall calculate_time
 
-    mov R1, #CLOCK			                ; PO TABLICY Z CYFRAMI CHODZIMY WSKAèNIKIEM R1
+    mov R1, #CLOCK			                ; PO TABLICY Z CYFRAMI CHODZIMY WSKA≈πNIKIEM R1
 
-    clr SEG7 			                    ;  W£•CZ 7-SEGMENT
-    mov R7, #00000001b 		                ; ZACZYNAMY OD NAJM£ODSZEGO WYåWIETLACZA
-    mov DPTR, #DISPLAY_PATTERNS       	    ; ADRES TABLICY PRZECHOWYWUJ•CEJ WZORY 7-SEGMENTOWEGO WYåWIETLACZA DO DPTR
+    clr SEG7 			                    ;  W≈ÅƒÑCZ 7-SEGMENT
+    mov R7, #00000001b 		                ; ZACZYNAMY OD NAJM≈ÅODSZEGO WY≈öWIETLACZA
+    mov DPTR, #DISPLAY_PATTERNS       	    ; ADRES TABLICY PRZECHOWYWUJƒÑCEJ WZORY 7-SEGMENTOWEGO WY≈öWIETLACZA DO DPTR
 
   mov IE, #0				                ; BLOKUJEMY WSZYSTKIE PRZERWANIA
     mov TMOD, #01110000b		            ; BLOKADA TIMER1, TIMER0 W TRYBIE 0
-    mov TH0, #224           	            ; 900 PRZERWA— NA SEKUND  CO 1024 CYKLE MASZYNOWE
+    mov TH0, #224           	            ; 900 PRZERWA≈É NA SEKUNDƒò CO 1024 CYKLE MASZYNOWE
 
-    mov R6, #132            	            ; ZLICZANIE DO 900, ØEBY PIERWSZA SEKUNDA TRWA£A SEKUNDE
+    mov R6, #132            	            ; ZLICZANIE DO 900, ≈ªEBY PIERWSZA SEKUNDA TRWA≈ÅA SEKUNDƒò
     mov R5, #4
 
     setb TR0			                    ; ZGODA NA ZLICZANIE PRZEZ LICZNIKI TIMERA0
-    setb ET0			                    ; ZGODA NA OBS£UG  PRZERWANIA OD TIMERA0
-    setb EA				                    ; GLOBALNA ZGODA NA OBS£UG  PRZERWANIA
+    setb ET0			                    ; ZGODA NA OBS≈ÅUGƒò PRZERWANIA OD TIMERA0
+    setb EA				                    ; GLOBALNA ZGODA NA OBS≈ÅUGƒò PRZERWANIA
 
 main_loop:
     jnb F0, main_loop		                ; CZEKAMY NA PRZERWANIE
     clr F0				                    ; ZAPOMINAMY O PRZERWANIU
 
-    ; TUTAJ JESTEM 900 RAZY NA SEKUND 
+    ; TUTAJ JESTEM 900 RAZY NA SEKUNDƒò
 
     ; CYFRA
-    mov R0, #ACTIVE_DISPLAY_SEGMENTS_BUFFER ; ZA£ADUJ ADRES ACTIVE_DISPLAY_SEGMENTS_BUFFER DO R0
+    mov R0, #ACTIVE_DISPLAY_SEGMENTS_BUFFER ; ZA≈ÅADUJ ADRES ACTIVE_DISPLAY_SEGMENTS_BUFFER DO R0
     mov A, @R1             		            ; AKTUALNA CYFRA ZEGARA DO AKUMULATORA
-    inc R1                  	            ; W NAST PNYM OBROCE P TLI WEè NAST PN• CYFR 
-    setb SEG7               	            ; WY£•CZ 7-SEGMENTOWY WYåWIETLACZ (ANTI-GHOSTING)
-    movx @R0, A             	            ; WYSY£AMY WZOREK DO REJESTRU SEGMENT”W
+    inc R1                  	            ; W NASTƒòPNYM OBROCIE PƒòTLI WE≈π NASTƒòPNƒÑ CYFRƒò
+    setb SEG7               	            ; WY≈ÅƒÑCZ 7-SEGMENTOWY WY≈öWIETLACZ (ANTI-GHOSTING)
+    movx @R0, A             	            ; WYSY≈ÅAMY WZOREK DO REJESTRU SEGMENT√ìW
 
-    ; WYåWIETLACZ
-    mov R0, #ACTIVE_DISPLAY_BUFFER 		    ; ZA£ADUJ ADRES ACTIVE_DISPLAY_BUFFER DO R0
-    mov A, R7			                    ; AKTUALNY WYåWIETLACZ DO AKUMULATORA
-    movx @R0, A             	            ; WYBIERZ WYåWIETLACZ
-    clr SEG7                	            ; W£•CZ 7-SEGMENTOWY WYåWIETLACZ
+    ; WY≈öWIETLACZ
+    mov R0, #ACTIVE_DISPLAY_BUFFER 		    ; ZA≈ÅADUJ ADRES ACTIVE_DISPLAY_BUFFER DO R0
+    mov A, R7			                    ; AKTUALNY WY≈öWIETLACZ DO AKUMULATORA
+    movx @R0, A             	            ; WYBIERZ WY≈öWIETLACZ
+    clr SEG7                	            ; W≈ÅƒÑCZ 7-SEGMENTOWY WY≈öWIETLACZ
 
     jnb SKEY, no_key
     orl KEYBOARD, A
 
 no_key:
-    rl A                    	            ; W NAST PNYM OBROCIE P TLI WEè NAST PNY WYåWIETLACZ
-    mov R7, A			                    ; ZAPAMI TAJ W R7 NOWY WYåWIETLACZ
-    jnb ACC.7, noACC7 		                ; JEåLI BIT 7 = 0, SKOCZ DO noACC7
-    mov R7, #00000001b      	            ; JEåLI NIE, ZACZYNAM ZNOWU OD NAJM£ODSZEGO WYåWIETLACZA
-    mov R1, #CLOCK 			                ; WRACAMY NA POCZ•TEK TABLICY CLOCK
+    rl A                    	            ; W NASTƒòPNYM OBROCIE PƒòTLI WE≈π NASTƒòPNY WY≈öWIETLACZ
+    mov R7, A			                    ; ZAPAMIƒòTAJ W R7 NOWY WY≈öWIETLACZ
+    jnb ACC.7, noACC7 		                ; JE≈öLI BIT 7 = 0, SKOCZ DO noACC7
+    mov R7, #00000001b      	            ; JE≈öLI NIE, ZACZYNAM ZNOWU OD NAJM≈ÅODSZEGO WY≈öWIETLACZA
+    mov R1, #CLOCK 			                ; WRACAMY NA POCZƒÑTEK TABLICY CLOCK
 
-    ; DEBOUNCING KLAWIATURY, 3 TAKIE SAME, 1 NIE ABY ZAREAGOWA∆ TYLKO RAZ
+    ; DEBOUNCING KLAWIATURY, 3 TAKIE SAME, 1 NIE ABY ZAREAGOWAƒÜ TYLKO RAZ
     mov A, KEYBOARD
     cjne A, KEYBOARD+1, unstable
     cjne A, KEYBOARD+2, unstable
@@ -101,23 +101,23 @@ unstable:
     mov KEYBOARD+3, KEYBOARD+2
     mov KEYBOARD+2, KEYBOARD+1
     mov KEYBOARD+1, KEYBOARD
-    mov KEYBOARD, #0            	        ; SK£ADAJ KLAWIATURE OD POCZ•TKU
+    mov KEYBOARD, #0            	        ; SK≈ÅADAJ KLAWIATURƒò OD POCZƒÑTKU
 
 noACC7:
-    djnz R6, main_loop      	            ; SKOCZ DO main_loop JEåLI R6 NIE JEST ZEREM
-    djnz R5, main_loop      	            ; SKOCZ DO main_loop JEåLI R5 NIE JEST ZEREM
+    djnz R6, main_loop      	            ; SKOCZ DO main_loop JE≈öLI R6 NIE JEST ZEREM
+    djnz R5, main_loop      	            ; SKOCZ DO main_loop JE≈öLI R5 NIE JEST ZEREM
 
     mov R6, #132            	            ; ZLICZANIE DO 900 (SEKUNDA)
     mov R5, #4
 
-    ; TUTAJ JESTEM CO SEKUND 
-    inc SS        			                ; ZWI KSZ SEKUNDY
+    ; TUTAJ JESTEM CO SEKUNDƒò
+    inc SS        			                ; ZWIƒòKSZ SEKUNDY
 
-    ; ZWI KSZANIE MINUT / GODZIN PRZY PRZEPE£NIENIU
+    ; ZWIƒòKSZANIE MINUT / GODZIN PRZY PRZEPE≈ÅNIENIU
     mov A, SS
-    cjne A, #60, no60      	                ; JEåLI SEKUNDY NIE S• R”WNE 60, POMIJAMY
-    mov SS, #0              	            ; JEåLI S• R”WNE 60, ZERUJEMY JE
-    inc MM           		                ; I ZWI KSZAMY MINUTY
+    cjne A, #60, no60      	                ; JE≈öLI SEKUNDY NIE SƒÑ R√ìWNE 60, POMIJAMY
+    mov SS, #0              	            ; JE≈öLI SƒÑ R√ìWNE 60, ZERUJEMY JE
+    inc MM           		                ; I ZWIƒòKSZAMY MINUTY
 
     mov A, MM
     cjne A, #60, no60
@@ -132,7 +132,7 @@ no60:
     lcall calculate_time
     sjmp main_loop
 
-keyboard_handler:         		            ; ZAK£ADAMY, ØE STAN KLAWIATURY JEST W AKUMULATORZE
+keyboard_handler:         		            ; ZAK≈ÅADAMY, ≈ªE STAN KLAWIATURY JEST W AKUMULATORZE
         cjne A, #100010b, no_left_esc
         mov A, HH
         cjne A, #23, add_hours
@@ -205,37 +205,37 @@ no_up:
 calculate_time:                   		    ; PRZELICZANIE LICZB Z HH, MM i SS NA CYFRY DLA TABLICY CLOCK
     mov DPTR, #DISPLAY_PATTERNS
 
-    mov A, SS			                    ; ZA£ADUJ DO AKUMULATORA LICZNIK SEKUND
+    mov A, SS			                    ; ZA≈ÅADUJ DO AKUMULATORA LICZNIK SEKUND
     mov B, #10
     div AB				                    ; PODZIEL SEKUNDY PRZEZ 10
-    movc A, @A+DPTR         	            ; POBIERAMY WZ”R CYFRY DZIESI•TEK SEKUND Z PAMI CI ROM
+    movc A, @A+DPTR         	            ; POBIERAMY WZ√ìR CYFRY DZIESIƒÑTEK SEKUND Z PAMIƒòCI ROM
     mov CLOCK+1, A          	            ; UMIESZCZAMY GO W ODPOWIEDNIM MIEJSCU
     mov A, B                	            ; JEDNOSTKI SEKUND DO AKUMULATORA
-    movc A, @A+DPTR         	            ; POBIERAMY WZ”R CYFRY JEDNOSTEK SEKUND Z PAMI CI ROM
+    movc A, @A+DPTR         	            ; POBIERAMY WZ√ìR CYFRY JEDNOSTEK SEKUND Z PAMIƒòCI ROM
     mov CLOCK, A            	            ; UMIESZCZAMY GO W ODPOWIEDNIM MIEJSCU
 
-    mov A, MM			                    ; ZA£ADUJ DO AKUMULATORA LICZNIK MINUT
+    mov A, MM			                    ; ZA≈ÅADUJ DO AKUMULATORA LICZNIK MINUT
     mov B, #10
     div AB				                    ; PODZIEL MINUTY PRZEZ 10
-    movc A, @A+DPTR         	            ; POBIERAMY WZ”R CYFRY DZIESI•TEK MINUT Z PAMIECI ROM
+    movc A, @A+DPTR         	            ; POBIERAMY WZ√ìR CYFRY DZIESIƒÑTEK MINUT Z PAMIƒòCI ROM
     mov CLOCK+3, A          	            ; UMIESZCZAMY GO W ODPOWIEDNIM MIEJSCU
     mov A, B                	            ; JEDNOSTKI MINUT DO AKUMULATORA
-    movc A, @A+DPTR         	            ; POBIERAMY WZ”R CYFRY JEDNOSTEK MINUT Z PAMI CI ROM
+    movc A, @A+DPTR         	            ; POBIERAMY WZ√ìR CYFRY JEDNOSTEK MINUT Z PAMIƒòCI ROM
     mov CLOCK+2, A          	            ; UMIESZCZAMY GO W ODPOWIEDNIM MIEJSCU
 
-    mov A, HH			                    ; ZA£ADUJ DO AKUMULATORA LICZNIK GODZIN
+    mov A, HH			                    ; ZA≈ÅADUJ DO AKUMULATORA LICZNIK GODZIN
     mov B, #10
     div AB				                    ; PODZIEL GODZINY PRZEZ 10
-    movc A, @A+DPTR         	            ; POBIERAMY WZ”R CYFRY DZIESI•TEK GODZIN Z PAMI CI ROM
+    movc A, @A+DPTR         	            ; POBIERAMY WZ√ìR CYFRY DZIESIƒÑTEK GODZIN Z PAMIƒòCI ROM
     mov CLOCK+5, A          	            ; UMIESZCZAMY GO W ODPOWIEDNIM MIEJSCU
     mov A, B                	            ; JEDNOSTKI GODZIN DO AKUMULATORA
-    movc A, @A+DPTR         	            ; POBIERAMY WZ”R CYFRY JEDNOSTEK GODZIN Z PAMI CI ROM
+    movc A, @A+DPTR         	            ; POBIERAMY WZ√ìR CYFRY JEDNOSTEK GODZIN Z PAMIƒòCI ROM
     mov CLOCK+4, A          	            ; UMIESZCZAMY GO W ODPOWIEDNIM MIEJSCU
 
-    ret                     	            ; POWR”T Z FUNKCJI
+    ret                     	            ; POWR√ìT Z FUNKCJI
 
 DISPLAY_PATTERNS:
-;DEFINICJE WZOR”W WYåWIETLACZA 7-SEGMENTOWEGO PREZECHOWYWANA W PAMI CI ROM
+;DEFINICJE WZOR√ìW WY≈öWIETLACZA 7-SEGMENTOWEGO PRZECHOWYWANA W PAMIƒòCI ROM
 db 00111111b, 00000110b, 01011011b, 01001111b
 db 01100110b, 01101101b, 01111101b, 00000111b
 db 01111111b, 01101111b, 01110111b, 01111100b
